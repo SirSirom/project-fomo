570d76b35eb04b9911e642357ec93715afe564c7
Marten Meichsner <meichsner.marten+github@proton.me>
2025-09-24 13:56:10 +0200

    +Kommentare, -Beispielcode

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 
 const String mockjsonDataString = '''
 {
-  "Option1": {
+  "Course1": {
     "name": "Fotografie-Workshop",
     "infoUrl": "https://info.url",
     "seats": 20,
@@ -18,7 +18,7 @@ const String mockjsonDataString = '''
       "Option2"
     ]
   },
-  "Option2": { 
+  "Course2": { 
     "name": "Videobearbeitungs-Kurs",
     "infoUrl": "https://info.url",
     "seats": 20,
@@ -28,7 +28,7 @@ const String mockjsonDataString = '''
       "Option1"
     ]
   },
-  "Option3": {
+  "Course3": {
     "name": "Grundlagen der Webentwicklung",
     "infoUrl": "https://info.url",
     "seats": 20,
@@ -55,18 +55,18 @@ class CourseOption {
     required this.exclusive,
   });
 
-  // Eine "Factory"-Methode, um ein CourseOption-Objekt aus JSON zu erstellen.
+  // Eine "Factory"-Methode, um ein CourseOption-Objekt aus dem JSON zu erstellen.
   factory CourseOption.fromJson(String id, Map<String, dynamic> json) {
     return CourseOption(
       id: id,
-      name: json['name'] ?? 'Unbenannte Option',
-      seats: json['seats'] ?? 0,
-      taken: json['taken'] ?? 0,
-      exclusive: List<String>.from(json['exclusive'] ?? []),
+      name: json['name'] ?? 'Unbenannter Kurs', //als Standard "Unbekannter Kurs"
+      seats: json['seats'] ?? 0, //als Standard 0
+      taken: json['taken'] ?? 0, //als Standard 0
+      exclusive: List<String>.from(json['exclusive'] ?? []), //als Standard leer
     );
   }
 
-  // Ein Helfer, um zu prüfen, ob die Option ausgebucht ist.
+  // Ein Helfer, um zu prüfen, ob der Kurs ausgebucht ist
   bool get isFull => taken >= seats;
 }
 
@@ -78,27 +78,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Optionsauswahl',
+      title: 'Wahlpflichtkurswahl',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const OptionSelectionScreen(),
+      home: const CourseSelectionScreen(),
     );
   }
 }
 
-class OptionSelectionScreen extends StatefulWidget {
-  const OptionSelectionScreen({super.key});
+class CourseSelectionScreen extends StatefulWidget {
+  const CourseSelectionScreen({super.key});
 
   @override
-  State<OptionSelectionScreen> createState() => _OptionSelectionScreenState();
+  State<CourseSelectionScreen> createState() => _CourseSelectionScreenState();
 }
 
-class _OptionSelectionScreenState extends State<OptionSelectionScreen> {
-  // Liste aller verfügbaren Optionen
+class _CourseSelectionScreenState extends State<CourseSelectionScreen> {
+  // Liste aller verfügbaren Kurse
   List<CourseOption> _allOptions = [];
-  // Ein Set, um die IDs der ausgewählten Optionen zu speichern.
+  // Ein Set, um die IDs der ausgewählten Kurse zu speichern.
   // Ein Set ist hier effizient, da es keine Duplikate erlaubt.
   final Set<String> _selectedOptionIds = {};
   final int _maxSelections = 2;
@@ -165,30 +165,41 @@ class _OptionSelectionScreenState extends State<OptionSelectionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      //app bar
       appBar: AppBar(
-        title: const Text('Wähle bis zu 2 Optionen'),
+        title: const Text('Bitte wählen Sie Ihre Wahlpflichmodule aus.'),
       ),
+
+      //body
       body: Column(
         children: [
+
+          //x von x ausgewählt Text
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Text(
-              '${_selectedOptionIds.length} von $_maxSelections ausgewählt',
+              '${_selectedOptionIds.length} von $_maxSelections Modulen ausgewählt',
               style: Theme.of(context).textTheme.headlineSmall,
             ),
           ),
+
+          //liste der module
           Expanded(
-            child: ListView.builder(
+            child: ListView.builder( //ListView war die simpleste umsetzung
               itemCount: _allOptions.length,
               itemBuilder: (context, index) {
                 final option = _allOptions[index];
                 final isSelected = _selectedOptionIds.contains(option.id);
                 final isEnabled = _isOptionEnabled(option);
 
+                // einzelnes modul
                 return Card(
                   margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 6.0),
                   color: isEnabled ? Colors.white : Colors.grey[300],
                   child: ListTile(
+
+                    // icon, TODO: ICON aus der übergebenen icon-URL nutzen (evtl)
                     leading: CircleAvatar(
                       backgroundColor: option.isFull ? Colors.red.shade100 : Colors.green.shade100,
                       child: Icon(
@@ -196,20 +207,26 @@ class _OptionSelectionScreenState extends State<OptionSelectionScreen> {
                         color: option.isFull ? Colors.red : Colors.green,
                       ),
                     ),
+
+                    // titel
                     title: Text(option.name),
+
+                    //anz. plätze
                     subtitle: Text(
                         option.isFull
                             ? 'Ausgebucht'
                             : 'Freie Plätze: ${option.seats - option.taken}'
                     ),
+
+                    //selection checkbox
                     trailing: Checkbox(
                       value: isSelected,
-                      // Das Checkbox wird deaktiviert, indem onChanged auf null gesetzt wird.
+                      // die checkbox wird deaktiviert indem onChanged auf null gesetzt wird
                       onChanged: isEnabled
                           ? (bool? value) => _handleSelection(option)
                           : null,
                     ),
-                    // Die gesamte Kachel wird klickbar gemacht.
+                    // die gesamte kachel wird klickbar gemacht (TODO: gesamter Eintrag oder nur checkbox klickbar machen?)
                     onTap: isEnabled ? () => _handleSelection(option) : null,
                   ),
                 );
@@ -218,97 +235,6 @@ class _OptionSelectionScreenState extends State<OptionSelectionScreen> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-      ApiHelper.loadModules(4).then((value) {
-        value.forEach((module) {
-          print(module.toString());
-        });
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
